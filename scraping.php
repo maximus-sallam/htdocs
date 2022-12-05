f07f09f99539d8450a1487564a78f7796d7d7dd5
maximus <maximus.sallam@gmail.com>
2022-12-05 16:31:25 -0600

    Added scraping functionality.

---
+++ scraping.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Bring It!</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<a href="index.html">
+								<span class="icon fa-diamond"></span>
+							</a>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>Bring It!</h1>
+									<form action="action.php" method="post">
+									<p>URL from bringatrailer.com: <input type="text" name="URL" /></p>
+									<p><input type="submit" /></p>
+									</form>
+								</p>
+							</div>
+						</div>
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">&copy; Maximus Sallam. </br>Design by <a href="https://maximus-sallam.com">Maximus Sallam</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+			<script type="text/javascript" src="assets/js/skel.min.js"></script>
+			<script type="text/javascript" src="assets/js/util.js"></script>
+			<script type="text/javascript" src="assets/js/main.js"></script>
+
+	</body>
+</html>
